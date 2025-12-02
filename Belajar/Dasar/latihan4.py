6cd05b4d47f5f91f72abb5b17e35a8ab8a804fe4
RyanPy <ryanbayu1233@gmail.com>
2025-12-02 15:34:37 +0700

    Add files via upload

---
+++ Belajar/Dasar/latihan4.py
@@ -0,0 +1,9 @@
+with open("/storage/emulated/0/Download/Coding/SinauPy/Sinau/catatan.txt", "w") as f:
+  f.write("Aku sayang Sora!\n")
+with open("/storage/emulated/0/Download/Coding/SinauPy/Sinau/catatan.txt", "a") as f:
+  f.write("Aku sayang banget sama Sora!\n")
+with open("/storage/emulated/0/Download/Coding/SinauPy/Sinau/catatan.txt", "r") as f:
+  isi = f.read()
+  print("Isi file:\n", isi)
+  
+  

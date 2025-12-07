b728d70f758343fb53f7738240313ced10f12e86
RyanPy <ryanbayu1233@gmail.com>
2025-12-07 16:05:58 +0700

    Add files via upload

---
+++ CobaCoba/Kalkulator.py
@@ -0,0 +1,7 @@
+while True:
+      print("Selamat datang di Kalkulator!")
+      angka1 = int(input("Masukkan angka pertama: "))
+      angka2 = int(input("Masukkan angka kedua: "))
+  
+      hasil = angka1 * angka2
+      print(f"Jadi {angka1}×{angka2}={hasil}")
